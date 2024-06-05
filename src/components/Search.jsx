ea305afd6c5e1bc9fd90bdf44fa012b1ed9400fa
Ohm Patel <ohm2003.patel@gmail.com>
2024-06-04 23:56:12 -0400

    chore: Create chat and user chats if they don't exist

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -43,11 +43,43 @@ const Search = () => {
   };
 
   const handleSelect = async () => {
-    // Code to handle selection
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+
+    setUser(null);
+    setUsername("")
   };
 
-  const handleClear = () => {
-  }
 
   return (
     <Box sx={{ p: 2 }}>
